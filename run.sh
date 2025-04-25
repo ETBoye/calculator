e54e80cd74a7cb200740b7d32135041ee8bf0142
etboye <boye.esben@gmail.com>
2025-04-25 15:45:49 +0200

    Add password protection for dozzle

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+set -e
 if [ -z "${POSTGRES_USER}" ]; then
     echo "You need to set the POSTGRES_USER environment variable to run the application"
     exit
@@ -5,6 +6,13 @@ fi
 
 if [ -z "${POSTGRES_PASSWORD}" ]; then
     echo "You need to set the POSTGRES_PASSWORD environment variable to run application"
+    exit
+fi
+
+
+if [ ! -e nginx/.htpasswd ]
+then
+    echo "No file nginx/.htpasswd exist. You can copy .htpasswd.unsafe-admin-admin for a user with username: admin and password: admin for testing"
     exit
 fi
 
