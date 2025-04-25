fceaf8578470fe52309cc567cd07993d844811ca
etboye <boye.esben@gmail.com>
2025-04-25 16:22:47 +0200

    Add gin mode

--- run.sh
+++ run.sh
@@ -2,6 +2,22 @@
 
 set -e
 
+
+case $APP_ENV in
+    prod)
+        GIN_MODE=release
+        ;;
+    test)
+        GIN_MODE=debug
+        ;;
+    *)
+        echo "Please set environment variable APP_ENV to either test or prod"
+        exit
+        ;;
+esac
+
+
+# Some people have different commands for docker compose
 if command -v docker-compose 2>&1 >/dev/null;then
     DOCKER_COMPOSE_COMMAND="docker-compose"
 else
@@ -26,4 +42,4 @@ then
 fi
 
 $DOCKER_COMPOSE_COMMAND down
-$DOCKER_COMPOSE_COMMAND up -d
+GIN_MODE=$GIN_MODE $DOCKER_COMPOSE_COMMAND up -d
